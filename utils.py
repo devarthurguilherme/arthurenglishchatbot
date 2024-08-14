24b40d7d7560d94e7580f24d1fa68a1228b9f39e
Arthur Guilherme <104983447+devarthurguilherme@users.noreply.github.com>
2024-08-14 07:58:27 +0100

    fixing gitignore file

---
+++ utils.py
@@ -0,0 +1,42 @@
+import asyncio
+import edge_tts
+import streamlit as st
+import tempfile
+import speech_recognition as sr
+
+import io
+
+VOICES = ['en-US-GuyNeural', 'en-US-JennyNeural']
+
+
+async def generate_audio(text, voice):
+    """Gera um arquivo de áudio a partir do texto usando a voz selecionada."""
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3') as temp_file:
+        temp_file_path = temp_file.name
+        communicate = edge_tts.Communicate(text, voice)
+        await communicate.save(temp_file_path)
+    return temp_file_path
+
+
+def generateAndDisplay_audio(text, voice):
+    """Gera áudio a partir do texto e exibe o áudio em Streamlit."""
+    # Executa a geração de áudio de forma assíncrona
+    audio_file_path = asyncio.run(generate_audio(text, voice))
+
+    # Exibe o áudio em Streamlit
+    st.audio(audio_file_path, format='audio/mp3')
+
+
+def transcribe_audio(audio_buffer):
+    # Initialize recognizer
+    r = sr.Recognizer()
+    with sr.AudioFile(audio_buffer) as source:
+        audio_data = r.record(source)
+        try:
+            # Perform speech recognition
+            text = r.recognize_google(audio_data, language='pt-BR')
+            return text
+        except sr.RequestError as e:
+            return f"Could not request results; {e}"
+        except sr.UnknownValueError:
+            return "Unknown error occurred"
